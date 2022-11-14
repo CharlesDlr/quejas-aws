aaf5d0812858037a32d057f83fa6cb4dbe3203ed
CharlesDlr <carlosdlr7@hotmail.com>
2022-11-14 11:36:51 -0600

    Correcting DB credentials

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require ("pg").Pool
 const pool = new Pool({
     user: "postgres",
     password: "admin",
-    host: "localhost",
+    host: "database-1.cpbora9gtec8.us-east-1.rds.amazonaws.com",
     port: 5432,
     database: "quejasvl"
 });
