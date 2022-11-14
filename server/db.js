426448a432f2fca701aa400a5bbede1979b43ff4
CharlesDlr <carlosdlr7@hotmail.com>
2022-11-14 11:37:59 -0600

    Adding password for DB

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const Pool = require ("pg").Pool
 const pool = new Pool({
     user: "postgres",
-    password: "admin",
+    password: "password",
     host: "database-1.cpbora9gtec8.us-east-1.rds.amazonaws.com",
     port: 5432,
     database: "quejasvl"
